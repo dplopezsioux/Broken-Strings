677f059a9f9d0cf1246301c5a16eb99267627a7c
dplopezsioux <38842757+dplopezsioux@users.noreply.github.com>
2020-10-17 00:32:56 -0400

    The end

--- main.js
+++ main.js
@@ -7,3 +7,68 @@ console.log("Start");
 
 /** OUT "Secure"
 "No Secure" */
+
+var password = ["PASSWORDSECURE", "BLUEWORKING", "SECUREWORD"];
+
+var checkS = function (params) {
+  checkValue = true;
+
+  var CharBro = {
+    B: 0,
+    R: 0,
+    O: 0,
+    K: 0,
+    E: 0,
+    N: 0,
+  };
+
+  for (const key in params) {
+    switch (params[key]) {
+      case "B":
+        CharBro.B++;
+        break;
+
+      case "R":
+        CharBro.R++;
+        break;
+
+      case "O":
+        CharBro.O++;
+        break;
+
+      case "K":
+        CharBro.K++;
+        break;
+
+      case "E":
+        CharBro.E++;
+        break;
+
+      case "N":
+        CharBro.N++;
+        break;
+
+      default:
+        break;
+    }
+  }
+
+  if (
+    (CharBro.B == CharBro.R) |
+    (CharBro.R == CharBro.O) |
+    (CharBro.O == CharBro.K) |
+    (CharBro.K == CharBro.E) |
+    (CharBro.E == CharBro.N)
+  ) {
+    checkValue = false;
+  }
+  return checkValue;
+};
+
+password.forEach((element) => {
+  if (checkS(element)) {
+    console.log("Secure");
+  } else {
+    console.log("No Secure");
+  }
+});
