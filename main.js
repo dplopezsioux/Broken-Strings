50779e63043c2621f1e273b8b223e2f386b1acb9
dplopezsioux <38842757+dplopezsioux@users.noreply.github.com>
2020-10-16 23:56:05 -0400

    INT AND OUT
    INT AND OUT

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
-console.log("Start")
+console.log("Start");
 
+/* password: A-TO-Z */
+/**letters 'B', 'R', 'O', 'K', 'E' and 'N' */
 
+/** INT 1 <= T <= 1000  and 1 <= N <= 100 */
+
+/** OUT "Secure"
+"No Secure" */
